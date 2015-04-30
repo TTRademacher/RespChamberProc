bfb37834bb4f3716f00be2843125a88ee19888c8
Thomas Wutzler <thomas.wutzler@web.de>
2015-04-30 11:53:11 +0200

    plotCampaignConcSeries not execute example, generated Rd

--- R/plottingGG.R
+++ R/plottingGG.R
@@ -61,36 +61,28 @@ plotCampaignConcSeries <- function(
 	dev.off()
 }
 attr( plotCampaignConcSeries, "ex") <- function(){
-	load(file.path(projectDir,"SMANIEconc.Rd"))
-	ds0 <- ds <- SMANIEconc
-	#tmp <- subset(ds0, campaign==5 )
-	#ds0 <- ds <- subset(SMANIEconc, id %in% levels(SMANIEconc$id)[64+1:8] ) testing
-	#ds0 <- ds <- subset(SMANIEconc, id %in% c(levels(SMANIEconc$id)[64+1:3],"1.2.19","1.3.2")  ) #testing for fit errors
-	.tmp.f <- function(){
-		ds <- subset(ds0,  !id %in% c(
-						"1.2.19"	# nonunique times
-						,"1.3.2"	# exactly zero concentrations all
-				) )
+	if( FALSE ){		# depends on data not available with the package, but demonstrates the way
+		load(file.path(projectDir,"SMANIEconc.Rd"))
+		ds0 <- ds <- SMANIEconc
+		.createresCalcClosedChamberFlux <- function(){
+			# note how results of calcClosedChamberFlux are assembled in a list
+			resCalcClosedChamberFlux <- resL <- dlply( ds, .(id), function(dss){
+						cat( ",", levels(dss$id)[dss$id[1]] )
+						res2 <- res <- try( 
+								res <- calcClosedChamberFlux( dss, colTemp="AirTemp", volume = 0.6*0.6*0.6
+										, debugInfo = list(useOscarsLagDectect = TRUE, omitAutoCorrFit=TRUE, omitEstimateLeverage = TRUE)
+								)
+								,silent=TRUE)
+					})
+			save( resCalcClosedChamberFlux, file=file.path(projectDir,"resCalcClosedChamberFlux_SMANIEconc.Rd"))
+		}
+		load( file.path(projectDir,"resCalcClosedChamberFlux_SMANIEconc.Rd"))
+		resL <- resCalcClosedChamberFlux
+		names(resL)[ sapply( resL, inherits,  "try-error") ]	# "1.2.19"  "1.3.2"   "1.3.23"  "2.2.270" "2.2.277"
+		#
+		# plot to file
+		fileName=file.path(projectDir,paste0("SMANIEconc_CO2_dry.pdf"))	##<< fileName
+		plotCampaignConcSeries(ds, resL=resL, fileName=fileName)
 	}
-	#dss <- subset(ds, id == ds$id[1] )
-	#dss <- subset(ds, id == "1.3.2" )
-	# ds <- ds[-(1:33550), ]
-	.createresCalcClosedChamberFlux <- function(){
-		resCalcClosedChamberFlux <- resL <- dlply( ds, .(id), function(dss){
-					cat( ",", levels(dss$id)[dss$id[1]] )
-					res2 <- res <- try( 
-							res <- calcClosedChamberFlux( dss, colTemp="AirTemp", volume = 0.6*0.6*0.6
-									, debugInfo = list(useOscarsLagDectect = TRUE, omitAutoCorrFit=TRUE, omitEstimateLeverage = TRUE)
-							)
-							,silent=TRUE)
-				})
-		save( resCalcClosedChamberFlux, file=file.path(projectDir,"resCalcClosedChamberFlux_SMANIEconc.Rd"))
-	}
-	load( file.path(projectDir,"resCalcClosedChamberFlux_SMANIEconc.Rd"))
-	resL <- resCalcClosedChamberFlux
-	names(resL)[ sapply( resL, inherits,  "try-error") ]	# "1.2.19"  "1.3.2"   "1.3.23"  "2.2.270" "2.2.277"
-	#
-	fileName=file.path(projectDir,paste0("SMANIEconc_CO2_dry.pdf"))	##<< fileName
-	plotCampaignConcSeries(ds, resL=resL, fileName=fileName)
 }
 
