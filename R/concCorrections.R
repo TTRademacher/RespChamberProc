989d171b9662086d7f3b5fbb921a492bdf0e799b
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2017-06-09 12:02:35 +0200

    corrConcDilution: option isUpdateOnNonFiniteVapour
    setting this option to FALSE allows processing data where all data in
    vapour column is NA

--- R/concCorrections.R
+++ R/concCorrections.R
@@ -1,23 +1,34 @@
 
 corrConcDilution <- function(
-  ### Calculate concentration corrected for dilution with water vapor		
-  ds 					##<< data frame with each row one observations, and respective columns 
-  ,colConc="CO2_Avg"	##<< column name of CO2 concentration [ppm]
-  ,colVapour="H20_Avg"	##<< column name of water vapour concentration [ppt]
+		### Calculate concentration corrected for dilution with water vapor		
+		ds 					##<< data frame with each row one observations, and respective columns 
+		,colConc="CO2_Avg"	##<< column name of CO2 concentration [ppm]
+		,colVapour="H20_Avg"	##<< column name of water vapour concentration [ppt]
+		,isUpdateOnNonFiniteVapour=TRUE	##<< set to FALSE to not correct concentrations for records where vapour is not finite
+			##<< in the default, the concentration will be NA
 ){
-  ##details<< 
-  ##  If CO2 concentration is measured per moist air, this function will calculate the concentration\
-  ## per dry air.
-  
-  ##references<<
-  ## LI-COR, Application Note 129. The Importance of Water Vapor Measurements and Corrections. LI-COR, Inc., 4421 Superior Street, Lincoln, NE 68504, USA.
-  ds[,colConc]/(1- (ds[,colVapour])/1000)
-  ### numeric vector (nrow ds):  concentration of CO2 per dry air [ppm]
+	##details<< 
+	##  If CO2 concentration is measured per moist air, this function will calculate the concentration\
+	## per dry air.
+	##references<<
+	## LI-COR, Application Note 129. The Importance of Water Vapor Measurements and Corrections. LI-COR, Inc., 4421 Superior Street, Lincoln, NE 68504, USA.
+	vapourPpm <- ds[,colVapour]/1000
+	if( isTRUE(isUpdateOnNonFiniteVapour) ){
+		ds[,colConc]/(1-vapourPpm)
+	} else {
+		ans <- ds[,colConc]
+		isFiniteVapour <- is.finite(vapourPpm)
+		ans[isFiniteVapour] <- ans[isFiniteVapour]/(1-vapourPpm[isFiniteVapour])
+		ans
+	}
+	### numeric vector (nrow ds):  concentration of CO2 per dry air [ppm]
 } 
 attr(corrConcDilution,"ex") <- function(){
   #data(chamberLoggerEx1s)
   ds <- chamberLoggerEx1s
-  ds$CO2_dry <- corrConcDilution(ds)	
+  ds$H20_Avg[1L] <- NA
+  ds$CO2_dry <- corrConcDilution(ds); head(ds$CO2_dry)	
+  ds$CO2_dry <- corrConcDilution(ds, isUpdateOnNonFiniteVapour=FALSE); head(ds$CO2_dry)	
 }
 
 
